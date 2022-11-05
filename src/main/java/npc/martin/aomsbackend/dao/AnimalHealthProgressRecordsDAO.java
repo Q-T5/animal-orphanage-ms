703c7cb76f404a612d3edc93d836fe2eb654a753
Q-T5 <bikathimartin@outlook.com>
2022-11-05 10:08:12 +0300

    Renamed repo to DAO

--- src/main/java/npc/martin/aomsbackend/dao/AnimalHealthProgressRecordsDAO.java
+++ src/main/java/npc/martin/aomsbackend/dao/AnimalHealthProgressRecordsDAO.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface AnimalHealthProgressRecordsRepo extends MongoRepository<AnimalHealthProgressRecords, Integer> {
+public interface AnimalHealthProgressRecordsDAO extends MongoRepository<AnimalHealthProgressRecords, Integer> {
     
 }
