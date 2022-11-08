8be2e04eabe08969ecd027fffaa9898436ed027d
Q-T5 <bikathimartin@outlook.com>
2022-11-08 17:23:51 +0300

    Fixed issue with join tables and entity ids

--- src/main/java/npc/martin/aomsbackend/services/SystemUserDetailsServiceImpl.java
+++ src/main/java/npc/martin/aomsbackend/services/SystemUserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package npc.martin.aomsbackend.services;
 
 import npc.martin.aomsbackend.entity.SystemUser;
 import npc.martin.aomsbackend.repository.SystemUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,12 +18,15 @@ import org.springframework.stereotype.Service;
 public class SystemUserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     SystemUserRepository userRepository;
+    
+    private static final Logger logger = LoggerFactory.getLogger(SystemUserDetailsServiceImpl.class);
 
     @Override
     public UserDetails loadUserByUsername(String staffId) throws UsernameNotFoundException {
         SystemUser user = userRepository.findByStaffId(staffId)
             .orElseThrow(() -> new UsernameNotFoundException("Staff With That Id Not Found"));
         
+        logger.info("Found staff with id: {}", staffId);
         return SystemUserDetailsImpl.build(user);
     }
 }
