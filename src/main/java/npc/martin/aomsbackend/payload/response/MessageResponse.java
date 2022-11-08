54648d743c074017af5e60d7eaac6a8f1381e56d
Q-T5 <bikathimartin@outlook.com>
2022-11-08 13:23:32 +0300

    Created message response class

---
+++ src/main/java/npc/martin/aomsbackend/payload/response/MessageResponse.java
@@ -0,0 +1,20 @@
+package npc.martin.aomsbackend.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author bikathi_martin
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MessageResponse {
+    private String message;
+}
