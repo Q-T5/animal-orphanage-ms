3e239302953f8f2dacfa89cc6c7aa5666f3edf22
Q-T5 <bikathimartin@outlook.com>
2022-11-08 11:30:03 +0300

    Anotated the system user repositories

--- src/main/java/npc/martin/aomsbackend/repository/RoleRepository.java
+++ src/main/java/npc/martin/aomsbackend/repository/RoleRepository.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 import npc.martin.aomsbackend.entity.Role;
 import npc.martin.aomsbackend.entity.SystemRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author bikathi_martin
  */
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByRoleName(SystemRoles name);
 }
--- src/main/java/npc/martin/aomsbackend/repository/SystemUserRepository.java
+++ src/main/java/npc/martin/aomsbackend/repository/SystemUserRepository.java
@@ -3,11 +3,14 @@ package npc.martin.aomsbackend.repository;
 import java.util.Optional;
 import npc.martin.aomsbackend.entity.SystemUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author bikathi_martin
  */
+
+@Repository
 public interface SystemUserRepository extends JpaRepository<SystemUser, String> {
     Optional<SystemUser> findByStaffId(String staffId);
     Boolean existsByStaffId(String staffId);
