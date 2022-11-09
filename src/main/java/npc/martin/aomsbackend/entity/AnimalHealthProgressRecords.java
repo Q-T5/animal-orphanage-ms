04b492d44d95bebfd19181bdb79ac08c574709b1
Q-T5 <bikathimartin@outlook.com>
2022-11-09 13:46:09 +0300

    Rewrote file with Lombok

--- src/main/java/npc/martin/aomsbackend/entity/AnimalHealthProgressRecords.java
+++ src/main/java/npc/martin/aomsbackend/entity/AnimalHealthProgressRecords.java
@@ -3,6 +3,11 @@ package npc.martin.aomsbackend.entity;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,6 +18,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
  */
 
 @Document(collection = "healthprogressrecords")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimalHealthProgressRecords {
     @Id
     private Integer animalId;
@@ -20,62 +30,6 @@ public class AnimalHealthProgressRecords {
     private String whereFound;
     private String foods;
     private List<MedicalProgress> medicalProgress;
-
-    public AnimalHealthProgressRecords() {
-    }
-
-    public AnimalHealthProgressRecords(Integer animalId, ClinicalState clinicalState, String whereFound, String foods, List<MedicalProgress> medicalProgress) {
-        this.animalId = animalId;
-        this.clinicalState = clinicalState;
-        this.whereFound = whereFound;
-        this.foods = foods;
-        this.medicalProgress = medicalProgress;
-    }
-
-    public Integer getAnimalId() {
-        return animalId;
-    }
-
-    public void setAnimalId(Integer animalId) {
-        this.animalId = animalId;
-    }
-
-    public ClinicalState getClinicalState() {
-        return clinicalState;
-    }
-
-    public void setClinicalState(ClinicalState clinicalState) {
-        this.clinicalState = clinicalState;
-    }
-
-    public String getWhereFound() {
-        return whereFound;
-    }
-
-    public void setWhereFound(String whereFound) {
-        this.whereFound = whereFound;
-    }
-
-    public String getFoods() {
-        return foods;
-    }
-
-    public void setFoods(String foods) {
-        this.foods = foods;
-    }
-
-    public List<MedicalProgress> getMedicalProgress() {
-        return medicalProgress;
-    }
-
-    public void setMedicalProgress(List<MedicalProgress> medicalProgress) {
-        this.medicalProgress = medicalProgress;
-    }
-
-    @Override
-    public String toString() {
-        return "AnimalProgressRecords{" + "animalId=" + animalId + ", clinicalState=" + clinicalState + ", whereFound=" + whereFound + ", foods=" + foods + ", medicalProgress=" + medicalProgress + '}';
-    }
     
     public void updateMedicalProgress(MedicalProgress medicalProgressUpdate) {
         if(this.medicalProgress == null) {
@@ -86,90 +40,24 @@ public class AnimalHealthProgressRecords {
     }
 }
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 class ClinicalState {
     private Float weight;
     private Integer numberOfChildren;
     private String medicalState;
-
-    public ClinicalState(Float weight, Integer numberOfChildren, String medicalState) {
-        this.weight = weight;
-        this.numberOfChildren = numberOfChildren;
-        this.medicalState = medicalState;
-    }
-
-    public ClinicalState() {
-    }
-
-    public Float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(Float weight) {
-        this.weight = weight;
-    }
-
-    public Integer getNumberOfChildren() {
-        return numberOfChildren;
-    }
-
-    public void setNumberOfChildren(Integer numberOfChildren) {
-        this.numberOfChildren = numberOfChildren;
-    }
-
-    public String getMedicalState() {
-        return medicalState;
-    }
-
-    public void setMedicalState(String medicalState) {
-        this.medicalState = medicalState;
-    }
-
-    @Override
-    public String toString() {
-        return "ClinicalState{" + "weight=" + weight + ", numberOfChildren=" + numberOfChildren + ", medicalState=" + medicalState + '}';
-    }
 }
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 class MedicalProgress {
     private LocalDate checkupDate;
     private String checkupNotes;
     private LocalDate nextCheckupDate;
-
-    public MedicalProgress() {
-    }
-
-    public MedicalProgress(LocalDate checkupDate, String checkupNotes, LocalDate nextCheckupDate) {
-        this.checkupDate = checkupDate;
-        this.checkupNotes = checkupNotes;
-        this.nextCheckupDate = nextCheckupDate;
-    }
-
-    public LocalDate getCheckupDate() {
-        return checkupDate;
-    }
-
-    public void setCheckupDate(LocalDate checkupDate) {
-        this.checkupDate = checkupDate;
-    }
-
-    public String getCheckupNotes() {
-        return checkupNotes;
-    }
-
-    public void setCheckupNotes(String checkupNotes) {
-        this.checkupNotes = checkupNotes;
-    }
-
-    public LocalDate getNextCheckupDate() {
-        return nextCheckupDate;
-    }
-
-    public void setNextCheckupDate(LocalDate nextCheckupDate) {
-        this.nextCheckupDate = nextCheckupDate;
-    }
-
-    @Override
-    public String toString() {
-        return "MedicalProgress{" + "checkupDate=" + checkupDate + ", checkupNotes=" + checkupNotes + ", nextCheckupDate=" + nextCheckupDate + '}';
-    }
 }
