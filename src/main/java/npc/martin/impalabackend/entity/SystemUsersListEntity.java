436806243ef63e178abe7623f26642f3a7cf7a8c
Q-T5 <bikathimartin@outlook.com>
2022-11-15 17:12:29 +0300

    Removed unnecessary field from list entity

--- src/main/java/npc/martin/impalabackend/entity/SystemUsersListEntity.java
+++ src/main/java/npc/martin/impalabackend/entity/SystemUsersListEntity.java
@@ -17,14 +17,11 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-
-//builder design pattern is used here
 public class SystemUsersListEntity {
     private String staffId;
     private String firstName;
     private String lastName;
     private String password;
     private Boolean accountStatus;
-    private String recoveryPhrase;
     private List<String> staffRoles;
 }
